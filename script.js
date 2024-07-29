562d81b0d9bcf5b4dbdd5f04638089af7be98a74
jayveersingh3031 <jayveersinghpawar03@gmail.com>
2024-07-29 23:55:52 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+/*window.addEventListener("mousemove",function(dets){
+    document.querySelector(".smcircle").style.left=dets.clientX+"px";
+    document.querySelector(".smcircle").style.top=dets.clientY+"px";
+
+})*/
+
+
+
+console.log(2+2);
